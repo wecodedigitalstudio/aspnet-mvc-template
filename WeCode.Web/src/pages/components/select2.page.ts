f021ab159fdc607a6fbd056f348b78d5e8d9e9e3
Roberto Borsini <r.borsini@loccioni.com>
2018-04-24 16:25:48 +0200

    added select2 page ts

--- WeCode.Web/src/pages/components/select2.page.ts
+++ WeCode.Web/src/pages/components/select2.page.ts
@@ -1,30 +1,38 @@
-import Vue from "vue";
+﻿import Vue from "vue";
 import Select2 from "../../components/common/select2.vue";
-var Select2Page = /** @class */ (function () {
-    function Select2Page() {
+
+export class Select2Page {
+
+
+    constructor() {
+
         console.log("Select2 page");
-        var v = new Vue({
+
+        let v = new Vue({
             el: "#select2-demo",
-            components: { Select2: Select2 },
+            components: { Select2 },
             data: {
                 selectedCategory: 'aa',
                 categories: [],
+
                 selectedCountry: '4'
             },
             methods: {
-                onCategoryChanged: function (newValue) {
+                onCategoryChanged(newValue: any) {
                     console.log("new selected category", this.selectedCategory);
                 },
-                onCountryChanged: function (newValue) {
+                onCountryChanged(newValue: any) {
                     console.log("new selected country", this.selectedCountry);
                 },
-                onContentLoaded: function () {
+                onContentLoaded() {
                     console.log("categories loaed");
                 }
             }
         });
+
     }
-    return Select2Page;
-}());
-export { Select2Page };
+
+}
+
+
 var page = new Select2Page();
